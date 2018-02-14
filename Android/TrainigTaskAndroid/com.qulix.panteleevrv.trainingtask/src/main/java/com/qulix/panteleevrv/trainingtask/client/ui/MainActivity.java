c37ad74494130fa20c0a02be2d9a37f6caf9aa81
Roman Panteleev <romaxa95.95@gmail.com>
2018-02-14 12:34:42 +0300

    gitignore

--- Android/TrainigTaskAndroid/com.qulix.panteleevrv.trainingtask/src/main/java/com/qulix/panteleevrv/trainingtask/client/ui/MainActivity.java
+++ Android/TrainigTaskAndroid/com.qulix.panteleevrv.trainingtask/src/main/java/com/qulix/panteleevrv/trainingtask/client/ui/MainActivity.java
@@ -1,3 +1,7 @@
+package com.qulix.panteleevrv.trainingtask.client.ui;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
